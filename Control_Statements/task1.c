b00e6cbf44386a4bcdd823581cce7ea5f93390e1
Davit Melikyan <davidmelikyan900@gmail.com>
2025-03-22 18:40:02 +0400

    Issue correcting

--- Control_Statements/task1.c
+++ Control_Statements/task1.c
@@ -2,16 +2,17 @@
 
 int main() {
 	for (int i = 0; i <= 10; ++i) {
-	printf("%d \n",i);
+		printf("%d \n",i);
 	}
-	int a, b = 0;
+	int a = 0;
+	int b = 0;
 	while (a <= 10) {
-	printf("%d \n",a);
-	++a;
+		printf("%d \n",a);
+		++a;
 	}
 	do {
-	printf("%d \n", b);
-	++b;
+		printf("%d \n", b);
+		++b;
 	} while (b <= 10);
 
 	return 0;
